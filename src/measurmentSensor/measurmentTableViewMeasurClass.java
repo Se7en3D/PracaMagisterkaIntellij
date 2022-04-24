d94c5a37ff51acba500be6fd1618b9c4615476eb
Se7en3D <Ek"GZag8>
2022-04-24 11:44:44 +0200

    Interpolacja

--- src/measurmentSensor/measurmentTableViewMeasurClass.java
+++ src/measurmentSensor/measurmentTableViewMeasurClass.java
@@ -8,7 +8,7 @@ import static java.lang.Math.pow;
 public class measurmentTableViewMeasurClass {
     private int referenceMeasurment;
     private double ultrasonicSensor;
-    private int laserSensor;
+    private double laserSensor;
 
     public measurmentTableViewMeasurClass() {
     }
@@ -23,10 +23,17 @@ public class measurmentTableViewMeasurClass {
     public measurmentTableViewMeasurClass (int referenceMeasurment, ArrayList<Integer> frame){
         this.referenceMeasurment = referenceMeasurment;
         int tempInt=(frame.get(5)<<24) | (frame.get(4)<<16)|(frame.get(3)<<8)|(frame.get(2));
-        this.ultrasonicSensor=Float.intBitsToFloat(tempInt);
-        this.laserSensor=(frame.get(7)<<8)|(frame.get(6));
+        this.ultrasonicSensor=(0.0343*tempInt)/2;
+        this.ultrasonicSensor=this.ultrasonicSensor*1.0744-6.3270-this.ultrasonicSensor*this.ultrasonicSensor*0.00033439;
+        this.laserSensor=((double )((frame.get(7)<<8)|(frame.get(6))))/10;
+        /*System.out.println(this.laserSensor);
+        System.out.println((this.laserSensor*this.laserSensor*this.laserSensor*0.000041402));
+        System.out.println((this.laserSensor*this.laserSensor*0.0045));
+        System.out.println((this.laserSensor*1.1052));*/
+        this.laserSensor=(this.laserSensor*1.1052)-5.3326-(this.laserSensor*this.laserSensor*0.0045)+(this.laserSensor*this.laserSensor*this.laserSensor*0.000041402);
         //this.laserSensor =(float ) 0.00011899*this.laserSensor*this.laserSensor+(float) 0.90281*this.laserSensor-(float)64.5932;
-
+        this.laserSensor=Math.round(this.laserSensor * 100.0) / 100.0;
+        this.ultrasonicSensor=Math.round(this.ultrasonicSensor * 100.0) / 100.0;
     }
 
     public measurmentTableViewMeasurClass (ArrayList<Integer> frame){
